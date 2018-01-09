306409f77a152b3d405bfb06537d5d9b8b0e56e2
David Sanftenberg <david.sanftenberg@gmail.com>
2018-01-09 11:41:20 +0000

    More logging

--- oauthenticator/generic.py
+++ oauthenticator/generic.py
@@ -103,8 +103,16 @@ class GenericOAuthenticator(OAuthenticator):
         app_log.info("Headers are: %s", req.headers)
         app_log.info("Body is: %s", req.body)
         
-        resp = yield http_client.fetch(req)
-
+        try:
+            resp = yield http_client.fetch(req)
+            print(resp.body)
+        except httpclient.HTTPError as e:
+            # HTTPError is raised for non-200 responses; the response
+            # can be found in e.response.
+            app_log.error("Error during POST to OAuth provider token endpoint: %s", e)
+            app_log.error("Full response was: %s", e.response)
+            throw
+        
         resp_json = json.loads(resp.body.decode('utf8', 'replace'))
 
         access_token = resp_json['access_token']
